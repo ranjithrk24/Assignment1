078f51b88f61bb87a4a988208daf92315cb51b54
Ranjithkumar <109387685+ranjithrk24@users.noreply.github.com>
2022-07-17 14:17:01 +0530

    Update reverse.py

--- reverse.py
+++ reverse.py
@@ -1,22 +1,2 @@
 s='Edyoda'
 print(s[::-1])
-
-a=''
-for i in s:
-    a=i+a
-print(a)
-
-a=[]
-for i in s:
-    a.append(i)
-a.reverse()
-for i in a:
-    print(i,end='')
-print('')
-for o in reversed(s):
-    print(o,end='')
-
-
-    
-
-
